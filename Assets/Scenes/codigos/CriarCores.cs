8095e4b2bf0d0d62a5748458921cf1297bac74df
Ingrid P. Santos <21470391@anhembimorumbi.edu.br>
2021-10-11 01:01:32 -0300

    novoscodigos

--- Assets/Scenes/codigos/CriarCores.cs
+++ Assets/Scenes/codigos/CriarCores.cs
@@ -6,31 +6,102 @@ using corcor;
 
 public class CriarCores : MonoBehaviour
 {
-   
+    [SerializeField] private float distancia;
+    [SerializeField] private float timer;
+    private Cena cena;
+    private bool estaSelecionado;
     public Stack<Color> stack;
+    public bool Pintar { get; set; }
+    public int Index { get; set; }
 
     [SerializeField] private Image[] imagens;
-        
+
+    private void Awake()
+    {
+        cena = FindObjectOfType<Cena>();
+    }
     private void Start()
     {
         stack = new Stack<Color>();
         Imagem();
-       
     }
 
     private void Imagem()
     {
         foreach (var imagem in imagens)
         {
-            int index = Random.Range(0, InformacoesGerais.cores.Count);
-            Color cor = InformacoesGerais.cores[index];
-            imagem.color = cor;
-            InformacoesGerais.coresUsadas[index]++;
-            if (InformacoesGerais.coresUsadas[index] == 4)
+            
+            Color cor ;
+            if (Pintar)
             {
-                InformacoesGerais.cores.RemoveAt(index);
+                int index = Random.Range(0, InformacoesGerais.cores.Count);
+                cor = InformacoesGerais.cores[index];
+                InformacoesGerais.coresUsadas[index]++;
+                if (InformacoesGerais.coresUsadas[index] == 4)
+                {
+                    InformacoesGerais.cores.RemoveAt(index);
+                }
             }
+            else
+            {
+                cor = new Color(0, 0, 0, 0);
+            }
+            
+            imagem.color = cor;
         }
+    }
+    public void StartMover()
+    {
+        StartCoroutine(Mover());
+    }
+   
+    private IEnumerator Mover()
+    {
+        switch (estaSelecionado)
+        {
+            case false:
+                if (cena.IndexSelecionado == Index)
+                {
+                    for (int i = 0; i < distancia; i++)
+                    { 
+                        transform.position += Vector3.up;
+                        yield return new WaitForSeconds(timer);
+                    }
+
+                    cena.IndexSelecionado = Index;
+                    cena.ObjetoSelecionado = gameObject;  
+                    estaSelecionado = cena.EstaSelecionda = true;
+                }
+                else
+                {
+                    if (cena.ObjetoSelecionado.transform.position.x > transform.position.x)
+                    {
+                        for (var i = transform.position.x; i < cena.ObjetoSelecionado.transform.position.x - 100; i++)
+                        {
+                           cena.ObjetoSelecionado.transform.position += Vector3.left;  
+                        }
+                    }
+                    else
+                    {
+                        for (var i = cena.ObjetoSelecionado.transform.position.x; i < transform.position.x - 100; i++)
+                        {
+                            cena.ObjetoSelecionado.transform.position += Vector3.right;  
+                        }
+                    }
+                }
+                break;
+         
+            case true:
+                for (int i = 0; i < distancia; i++)
+                { 
+                    transform.position -= Vector3.up;
+                    yield return new WaitForSeconds(timer);
+                }
+                estaSelecionado = cena.EstaSelecionda = false;
+                break;
+        }
+
+        
     }
 }
 
